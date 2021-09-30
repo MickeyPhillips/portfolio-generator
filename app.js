58418ef40513a05429a1c905e38fd41640520101
Mickey Phillips <phillipsmickey41@gmail.com>
2021-09-30 16:55:30 -0500

    added validation

--- app.js
+++ app.js
@@ -16,12 +16,30 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            }
+            else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
       },
       {
           type: 'input',
           name: 'github',
-          message: 'Enter your GitHub Username'
+          message: 'Enter your GitHub Username',
+          validate: gitUserInput => {
+              if (gitUserInput) {
+                  return true;
+              }
+              else {
+                  console.log('Please enter your GitHub Username!');
+                  return false;
+              }
+          }
       },
       {
           type: 'input',
@@ -47,12 +65,30 @@ Add a New Project
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter the project name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescInput => {
+                if (projectDescInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a project description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -63,7 +99,16 @@ Add a New Project
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: projectLinkInput => {
+                if (projectLinkInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a project link!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
