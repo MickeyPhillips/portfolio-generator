715ed7dcaa930910a91cef6b43293581618cdf9c
Mickey Phillips <phillipsmickey41@gmail.com>
2021-10-01 17:54:41 -0500

    Updated Template

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const inquirer = require("inquirer");
 
@@ -150,5 +150,11 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData)
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw err;
+  
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
